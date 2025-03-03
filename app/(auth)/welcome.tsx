50761cadaf8dbfd4d6a2211cf9c969e5bc700080
SteveNM <snrmiranda123@gmail.com>
2025-03-04 00:11:09 +0530

    Changing CodeBase 3

---
+++ app/(auth)/welcome.tsx
@@ -0,0 +1,46 @@
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import ScreenWrapper from "@/components/ScreenWrapper";
+import Typo from "@/components/Typo";
+import { colors, spacingX } from "@/constants/theme";
+import { verticalScale } from "@/utils/styling";
+
+const Welcome = () => {
+  return (
+    <ScreenWrapper>
+      <View style={styles.container}>
+        <View>
+          <TouchableOpacity style={styles.SignInButton}>
+            <Typo fontWeight={"500"}>Sign In</Typo>
+          </TouchableOpacity>
+
+          <Image source={require("../../assets/images/splashImage.png")}
+          style={styles.welcomeImage} 
+          resizeMode="contain"/>
+        </View>
+      </View>
+    </ScreenWrapper>
+  );
+};
+
+export default Welcome;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.neutral900,
+  },
+
+  SignInButton: {
+    alignSelf: "flex-end",
+    marginRight: spacingX._20,
+  },
+
+  welcomeImage: {
+    width:"100%",
+    height: verticalScale(150),
+    alignSelf:"center",
+    marginTop:verticalScale(200)
+
+  }
+});
---
+++ app/index.tsx
@@ -0,0 +1,45 @@
+import { Image, StyleSheet, Text, View } from "react-native";
+import React, { useEffect } from "react";
+import { colors } from "@/constants/theme";
+import { useRouter } from "expo-router";
+import { StatusBar } from "react-native";
+
+const index = () => {
+  const router = useRouter();
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      router.push("/(auth)/welcome");
+    }, 1500);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <StatusBar
+        backgroundColor="black"
+        barStyle="light-content"
+        translucent={false}
+      />
+      <Image
+        style={styles.logo}
+        resizeMode="contain"
+        source={require("../assets/images/splashImage.png")}
+      />
+    </View>
+  );
+};
+
+export default index;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: colors.neutral900,
+  },
+
+  logo: {
+    height: "50%",
+    aspectRatio: 0.5,
+  },
+});
