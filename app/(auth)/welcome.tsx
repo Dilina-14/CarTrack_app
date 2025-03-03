84d6c7b3066175ba59ee257c89256eaae9dc6c2d
SteveNM <snrmiranda123@gmail.com>
2025-03-04 01:07:32 +0530

    Updated welcome.tsx

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -11,7 +11,7 @@ const Welcome = () => {
       <View style={styles.container}>
         <View>
           <TouchableOpacity style={styles.SignInButton}>
-            <Typo fontWeight={"500"}>Sign Bye</Typo>
+            <Typo fontWeight={"500"}>Sign Bye test</Typo>
           </TouchableOpacity>
 
           <Image source={require("../../assets/images/splashImage.png")}
