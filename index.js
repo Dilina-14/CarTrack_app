dfc2a670cdcbb36bfb3fac473aa9b3d0220c9ae4
SteveNM <snrmiranda123@gmail.com>
2025-03-04 00:06:56 +0530

    Changing CodeBase 1

--- index.js
+++
@@ -1,6 +0,0 @@
-import { AppRegistry } from "react-native";
-import App from "./App";
-import MessageScreen from './MessageScreen'; 
-import {name as appName} from './app.json'
-
-AppRegistry.registerComponent(appName,() => MessageScreen)
