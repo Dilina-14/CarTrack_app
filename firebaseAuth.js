26f366523e6d081f5171d81bddc08dfe38430afc
disalHenuka <rmdhenuka12345@gmail.com>
2025-03-23 19:47:05 +0530

    add Item to marketplace

--- firebaseAuth.js
+++ firebaseAuth.js
@@ -22,5 +22,6 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
+const database = getDatabase(app);
 
-export { auth, db, app, storage };
+export { auth, db, database, app, storage };
