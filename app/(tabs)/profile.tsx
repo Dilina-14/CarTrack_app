b5165624abf30697365d5fd1827ca7912e3f0e44
Apurva <apurva.20222286@iit.ac.lk>
2025-03-24 07:25:54 +0530

    Update profile.tsx

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -212,7 +212,7 @@ const Profile = () => {
 
             <TouchableOpacity
               style={[styles.menuItem, { width: menuWidth }]}
-              onPress={() => router.push('../other/reminders')}
+              onPress={() => router.push('/other/reminders')}
             >
               <Image source={require('../../assets/images/reminders-icon.png')} />
               <Text style={[styles.menuText, { fontSize: fontSize }]}>Reminders</Text>
